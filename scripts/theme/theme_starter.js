d8cfceeafc04d6a0bf5094af55be7db3885d82e9
eduassi <eduassi@gmail.com>
2020-03-31 18:24:14 -0300

    Update theme_starter.js

--- scripts/theme/theme_starter.js
+++ scripts/theme/theme_starter.js
@@ -1,19 +1,22 @@
+var fire_once;
 var check_all_images_complete = function(callback){
     // We are listening for lazy images, when all images have a real size on DOM, we fire our callback function
+    try{clearTimeout(fire_once);}catch(e){}
     let images_container = $("#content-viewer").find("img");
     let number_images = images_container.length;
     let progress = 0;
-    
-    $(images_container).one("load", function() {
-        progress++;
-        if(progress >= number_images){
-            setTimeout(function(){
-                callback();
-                already_loaded = true;
-            },1000);
-        }
-    });   
-    
+    if($(images_container).length){
+        $(images_container).one("load", function() {
+            progress++;
+            if(progress >= number_images){
+                fire_once = setTimeout(function(){
+                    callback();
+                },1000);
+            }
+        });   
+    }else{
+        callback();
+    }
 }
 
 var initial_setup = function() {
